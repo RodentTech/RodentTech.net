666fb59a97d92c13b029dd64bc3dd3414093ffb9
zwang61 <54748426+zwang61@users.noreply.github.com>
2021-08-14 00:06:56 -0700

    Update utils by adding abstraction level

--- Code/src/util/Utils.java
+++ Code/src/util/Utils.java
@@ -97,3 +97,22 @@ public class Utils {
         }
     }
 }
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
